d19d983b40d7f00a2a715a6e442acb9dac0e60fd
Gosfist <akunsekolah1234567@gmail.com>
2025-02-20 19:17:28 +0700

    tes

--- hakAkses.java
+++ hakAkses.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class hakAkses {
-    public static void main (String[] args) {
+    
         Scanner input = new Scanner(System.in);
         int maxBuku = 100;
         int dataMaster = 10;
